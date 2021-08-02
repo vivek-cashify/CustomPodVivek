d38ef8f38de5736791210636cc786dab4d16bc51
Vivek <vivek.t@cashify.in>
2021-08-02 12:24:55 -0700

    Initial commit

---
+++ CustomPodVivek/CustomPodVivek/CustomPodVivek.h
@@ -0,0 +1,18 @@
+//
+//  CustomPodVivek.h
+//  CustomPodVivek
+//
+//  Created by Vivek Tyagi 1002852 on 02/08/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomPodVivek.
+FOUNDATION_EXPORT double CustomPodVivekVersionNumber;
+
+//! Project version string for CustomPodVivek.
+FOUNDATION_EXPORT const unsigned char CustomPodVivekVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomPodVivek/PublicHeader.h>
+
+
---
+++ CustomPodVivek/CustomPodVivek/ServiceClass.swift
@@ -0,0 +1,19 @@
+//
+//  ServiceClass.swift
+//  CustomPodVivek
+//
+//  Created by Vivek Tyagi 1002852 on 02/08/21.
+//
+
+import Foundation
+
+
+class ServiceClass {
+    
+    private init() {}
+    
+    public static func fun() -> String {
+        
+        return "HELLO MY FIRST COCOAPOD"
+    }
+}
